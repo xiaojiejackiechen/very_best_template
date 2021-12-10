3947e038381ea12f36bf1623e86b0bf0c1ed2a81
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 14:10:21 +0000

    Remove controller comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users
   def index
     @users = User.page(params[:page]).per(10)
   end
 
-  # GET /users/1
   def show
     @bookmark = Bookmark.new
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit; end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
@@ -30,7 +25,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       redirect_to @user, notice: "User was successfully updated."
@@ -39,7 +33,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
     redirect_to users_url, notice: "User was successfully destroyed."
@@ -47,12 +40,10 @@ class UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:username)
   end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,7 +1,6 @@
 class VenuesController < ApplicationController
   before_action :set_venue, only: %i[show edit update destroy]
 
-  # GET /venues
   def index
     @q = Venue.ransack(params[:q])
     @venues = @q.result(distinct: true).includes(:bookmarks, :neighborhood,
@@ -9,24 +8,19 @@ class VenuesController < ApplicationController
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(address_latitude: nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
-      marker.infowindow "<h5><a href='/venues/#{venue.id}'>#{venue.name}</a></h5><small>#{venue.address_formatted_address}</small>"
     end
   end
 
-  # GET /venues/1
   def show
     @bookmark = Bookmark.new
   end
 
-  # GET /venues/new
   def new
     @venue = Venue.new
   end
 
-  # GET /venues/1/edit
   def edit; end
 
-  # POST /venues
   def create
     @venue = Venue.new(venue_params)
 
@@ -42,7 +36,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /venues/1
   def update
     if @venue.update(venue_params)
       redirect_to @venue, notice: "Venue was successfully updated."
@@ -51,7 +44,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  # DELETE /venues/1
   def destroy
     @venue.destroy
     message = "Venue was successfully deleted."
@@ -64,12 +56,10 @@ class VenuesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_venue
     @venue = Venue.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def venue_params
     params.require(:venue).permit(:name, :address, :neighborhood_id)
   end
