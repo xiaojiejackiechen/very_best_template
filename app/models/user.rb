4eabb78c978619726bdb5f650250ddd04fb0e02d
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 14:08:02 +0000

    Generate users resource with devise

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    username
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
