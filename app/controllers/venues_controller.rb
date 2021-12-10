d97e6fb893a2a8160203df266d926cf848e5d3d6
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 14:09:56 +0000

    Install Kaminari

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,7 +3,7 @@ class VenuesController < ApplicationController
 
   # GET /venues
   def index
-    @venues = Venue.all
+    @venues = Venue.page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
