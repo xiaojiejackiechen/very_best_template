23ed80bcfd710782ac3c9175933a11d6a7062d4a
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 14:09:26 +0000

    Install graphiti

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
+    mount VandalUi::Engine, at: '/vandal'
+    # your routes go here
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root :to => "venues#index"
