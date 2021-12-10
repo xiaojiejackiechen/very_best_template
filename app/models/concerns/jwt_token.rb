c24b82f78a7b40ce13994072efecec51eaf01891
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-12-10 14:09:48 +0000

    Add JWT Concern.

---
+++ app/models/concerns/jwt_token.rb
@@ -0,0 +1,11 @@
+module JwtToken
+  extend ActiveSupport::Concern
+
+  def generate_jwt
+    JWT.encode({
+        id: id,
+        exp: 60.days.from_now.to_i
+      }, Rails.application.secrets.secret_key_base
+    )
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  # Direct associations
+  
+  include JwtToken
+# Direct associations
 
   has_many   :bookmarks,
              :dependent => :destroy
